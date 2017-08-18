c637f7fdd5d24e90d88a276eab40fca3fa6a38ef
Thamyres Guedes <thataguedis@gmail.com>
2017-08-18 16:27:33 -0300

    Moddleware

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response){
-	response.sendfile(__dirname + '/public/index.html');
+app.use(express.static('public'));
+
+app.get('/blocks', function(request, response){
+	var blocks = ['Fixed', 'Movable', 'Rotating'];
+	response.json(blocks);
 });
 
 app.listen(3000, function(){
