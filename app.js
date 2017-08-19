8182aa7dff4e400e3ff4d9f08d3f3d59b0add0ee
Thamyres Corrêa <thataguedis@gmail.com>
2017-08-19 18:39:54 -0300

    5.2 Router files

--- app.js
+++ app.js
@@ -1,59 +1,15 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false});
-
 var logger = require('./logger');
 app.use(logger);
 
 app.use(express.static('public'));
 
-var blocks = {
-	'Fixed':'Fastened securely in position', 
-	'Movable':'Capable of being moved', 
-	'Rotating':'Moving in a circle around its center'
-};
+var blocks = require('./routes/blocks');
+app.use('/blocks', blocks);
 
-app.param('name', function(request, response, next){
-	var name = request.params.name;
-	var block =  name[0].toUpperCase() + name.slice(1).toLowerCase();
 
-	request.blockName = block;
-
-	next();
-});
-
-app.route('/blocks')
-	.get(function(request, response){
-		if(request.query.limit >= 0){
-			response.json(Object.keys(blocks.slice(0, request.query.limit)));
-		} else {
-			response.json(Object.keys(blocks));
-		}
-	})
-	.post(parseUrlencoded, function(request, response){
-		var newBlock = request.body;
-		blocks[newBlock.name] = newBlock.description;
-
-		response.status(201).json(newBlock.name);
-	});
-
-app.route('/blocks/:name')
-	.delete(function(request, response){
-		delete blocks[request.blockName];
-		// Se colocar só status, ao invés de sendStatus o jquery não entende e não faz a ação do done.
-		response.sendStatus(200);
-	})
-	.get(function(request, response){
-		var descripton = blocks[request.blockName];
-		if(!descripton){
-			response.status(404).json("No description found for " +request.params.name);
-		} else {
-			response.json(descripton);
-		}
-	});
-	
 
 app.listen(3000, function(){
 	console.log('Listening on port 3000');
