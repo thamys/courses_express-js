745d96b78bab8d825e68f582d55243981bd58a99
Thamyres Corrêa <thataguedis@gmail.com>
2017-08-18 23:19:02 -0300

    Step 2 - Middlewares

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var logger = require('./logger');
+app.use(logger);
+
 app.use(express.static('public'));
 
 app.get('/blocks', function(request, response){
